a49e9d30e8e164d0c7fd50f8ab96109834f45872
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-02-15 12:34:47 +0000

    removed custom middleware

--- main.go
+++ main.go
@@ -6,14 +6,12 @@ import (
 	"github.com/go-chi/chi"
 
 	"github.com/UCCNetsoc/shortener/config"
-	"github.com/UCCNetsoc/shortener/middleware"
 	"github.com/UCCNetsoc/shortener/router"
 	"github.com/UCCNetsoc/shortener/views"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Mid)
 	router.Route(r)
 	config.InitConfig()
 	views.InitViews()
--- router/router.go
+++ router/router.go
@@ -1,18 +1,15 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/go-chi/chi"
-	mid "github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/middleware"
 
-	"github.com/UCCNetsoc/shortener/middleware"
 	"github.com/UCCNetsoc/shortener/views"
 )
 
 // Route ...
 func Route(r *chi.Mux) {
-	r.Use(mid.Logger)
+	r.Use(middleware.Logger)
 	r.Get("/{slug}", views.GetURL)
 	r.Post("/api", views.PostLink)
 	r.Delete("/api/{slug}", views.DeleteLink)
