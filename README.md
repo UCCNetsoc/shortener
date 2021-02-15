41c9a6120b5d886913c9e893d601147d9ceff524
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-02-15 12:18:51 +0000

    simplified router, updated readme

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 [![Build Status](https://ci.netsoc.co/api/badges/UCCNetsoc/shortener/status.svg)](https://ci.netsoc.co/UCCNetsoc/shortener)
 
 A URL shortener written in go
+
+|METHOD|PATH|Description|
+|--|--|--|
+|Get|/{slug}|HTTP Redirect to resolved URL from db|
+|POST|/api|Create slug/url pair on db {slug: string, url: string}|
+|DELETE|/api/{slug}|Delete slug/url pair on db|
--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 
 	"github.com/UCCNetsoc/shortener/middleware"
 	"github.com/UCCNetsoc/shortener/views"
@@ -11,15 +12,8 @@ import (
 
 // Route ...
 func Route(r *chi.Mux) {
+	r.Use(middleware.Logger)
 	r.Get("/{slug}", views.GetURL)
-	r.Mount("/api", apiHandler())
-}
-
-func apiHandler() http.Handler {
-	r := chi.NewRouter()
-
-	r.Use(middleware.Mid)
-	r.Post("/", views.PostLink)
-	r.Delete("/{slug}", views.DeleteLink)
-	return r
+	r.Post("/api", views.PostLink)
+	r.Delete("/api/{slug}", views.DeleteLink)
 }
